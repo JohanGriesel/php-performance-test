a1b7426675c8781580e58f7951a11620efe0a2be
Johan Griesel <johan@stratusolve.com>
2019-07-13 11:48:53 +0200

    Initial commit

---
+++ assets/php/config.php
@@ -0,0 +1,12 @@
+<?php
+$TestTypeStr = '';
+
+
+abstract class TestConfig {
+    const LIGHT_LOAD_STR = 'LIGHT_LOAD';
+    const MEDIUM_LOAD_STR = 'MEDIUM_LOAD';
+    const HEAVY_LOAD_STR = 'HEAVY_LOAD';
+
+    protected static $
+}
+?>
